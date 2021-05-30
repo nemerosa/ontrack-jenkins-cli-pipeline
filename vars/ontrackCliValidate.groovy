e6908cb954d888cb1900f7212f769e7f837ee850
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2021-05-30 17:55:17 +0200

    #4 Validation with explicit status

--- vars/ontrackCliValidate.groovy
+++ vars/ontrackCliValidate.groovy
@@ -1,9 +1,28 @@
-def call(Map<String,?> params = [:]) {
+import net.nemerosa.ontrack.jenkins.pipeline.utils.ParamUtils
+import net.nemerosa.ontrack.jenkins.pipeline.cli.Cli
+
+def call(Map<String, ?> params = [:]) {
 
     String project = ParamUtils.getParam(params, "project", env.ONTRACK_PROJECT_NAME as String)
     String branch = ParamUtils.getParam(params, "branch", env.ONTRACK_BRANCH_NAME as String)
     String build = ParamUtils.getParam(params, "build", env.ONTRACK_BUILD_NAME as String)
     String stamp = ParamUtils.getParam(params, "stamp")
+    String status = ParamUtils.getConditionalParam(params, "status", false, null)
     boolean logging = ParamUtils.getBooleanParam(params, "logging", false)
+
+    Closure logger = {}
+    if (logging) {
+        logger = {
+            println("[ontrack-cli-build] $it")
+        }
+    }
+
+    List<String> args = ['validate', '--project', project, '--branch', branch, '--build', build, '--validation', stamp]
+    if (status) {
+        args += '--status'
+        args += status
+    }
+
+    Cli.call(this, logger, args)
 
 }
--- vars/ontrackCliValidate.md
+++ vars/ontrackCliValidate.md
@@ -12,7 +12,14 @@ This step creates a validation run for the current Ontrack build, using the curr
 | `branch` | String | `ONTRACK_BRANCH_NAME` environment variable | Name of the branch in Ontrack to target |
 | `build` | String | `ONTRACK_BUILD_NAME` environment variable | Name of the build to validate in Ontrack |
 | `stamp` | String | _Required_ | Name of the validation stamp to set |
+| `status` | String | _None_ | Status of the validation. See the [validation](#validation) section below. |
 | `logging` | boolean | `false` | Set to `true` to display debug / logging information while performing the operation. |
+
+### Validation
+
+By default, the status of the validation is computed from the result of the current Jenkins stage.
+
+If can also be provided explicitly using the `status` parameter or through data validation.
 
 ### Remaining
 
