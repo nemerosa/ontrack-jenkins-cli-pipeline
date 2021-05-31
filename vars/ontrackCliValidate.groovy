39df923d43d184033acc0c0e2ac1c2ce18e3a43a
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2021-05-31 09:44:23 +0200

    #4 Run info - tracing

--- vars/ontrackCliValidate.groovy
+++ vars/ontrackCliValidate.groovy
@@ -43,7 +43,7 @@ def call(Map<String, ?> params = [:]) {
 
     // Run info
     RunInfo runInfo = JenkinsUtils.getRunInfo(this, tracer)
-    tracer("Run info = $runInfo")
+    tracer("Run info = ${runInfo.toString()}")
     if (runInfo != null && !runInfo.isEmpty()) {
         addArg(args, runInfo.runTime, '--run-time')
         addArg(args, runInfo.sourceType, '--source-type')
