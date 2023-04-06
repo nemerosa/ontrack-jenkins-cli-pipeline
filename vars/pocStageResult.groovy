eec3d9b134344e3b5b5fced84010341294586185
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-04-06 20:00:41 +0200

    [experimental] Using StageStatusStepExecutionMonitor (2.220)

--- vars/pocStageResult.groovy
+++ vars/pocStageResult.groovy
@@ -1,12 +1,18 @@
-import org.jenkinsci.plugins.workflow.actions.LabelAction
+import org.jenkinsci.plugins.workflow.cps.nodes.StepStartNode
+import org.jenkinsci.plugins.workflow.actions.StageStatusStepExecutionMonitor
 
 def call() {
     def stageName = env.STAGE_NAME
-    def actions = currentBuild.rawBuild.allActions
-    for (def action in actions) {
-        echo "Action: $action"
+    def build = currentBuild.rawBuild
+    def startNode = build.execution.nodes.find {
+        it instanceof StepStartNode &&
+                it.getDisplayName() == stageName
     }
-    def action = currentBuild.rawBuild.getAction(LabelAction.class)
-    String result = action ? action.getShortDescription() : "UNKNOWN"
-    echo "Result of stage '${stageName}': ${result}"
+    if (startNode != null) {
+        def result = startNode.getAction(StageStatusStepExecutionMonitor.Status.class)?.getResult()
+        if (result != null) {
+            println "Result of stage '${stageName}': ${result}"
+        }
+    }
+
 }
