a44219c2566bb7a555255a0c995cefa9ef1145dd
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-04-06 19:50:25 +0200

    [experimental] All actions

--- vars/pocStageResult.groovy
+++ vars/pocStageResult.groovy
@@ -2,6 +2,10 @@ import org.jenkinsci.plugins.workflow.actions.LabelAction
 
 def call() {
     def stageName = env.STAGE_NAME
+    def actions = currentBuild.rawBuild.allActions
+    for (def action in actions) {
+        echo "Action: $action"
+    }
     def action = currentBuild.rawBuild.getAction(LabelAction.class)
     String result = action ? action.getShortDescription() : "UNKNOWN"
     echo "Result of stage '${stageName}': ${result}"
