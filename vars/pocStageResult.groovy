5c51ea278374d4d68dcf80a415483cefdcc4974f
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-04-06 20:08:15 +0200

    [experimental]

--- vars/pocStageResult.groovy
+++ vars/pocStageResult.groovy
@@ -1,5 +1,4 @@
 import org.jenkinsci.plugins.workflow.cps.nodes.StepStartNode
-import org.jenkinsci.plugins.workflow.actions.StageStatusAction
 
 def call() {
     def stageName = env.STAGE_NAME
@@ -9,7 +8,10 @@ def call() {
                 it.getDisplayName() == stageName
     }
     if (startNode != null) {
-        def result = startNode.getAction(StageStatusAction.class)?.getResult()
+        for (def action in startNode.allActions) {
+            println("action = $action")
+        }
+        def result = startNode.getAction(org.jenkinsci.plugins.workflow.actions.StageStatusAction.class)?.getResult()
         if (result != null) {
             println "Result of stage '${stageName}': ${result}"
         }
